7f7433185b17a290d91e51952d3b3a76f84d00ff
MonoHaru <rlaxorud970@gmail.com>
2025-05-14 17:46:12 +0900

    add ch04 code

---
+++ common/gridworld.py
@@ -0,0 +1,84 @@
+import numpy as np
+import common.gridworld_render as render_helper
+
+
+class GridWorld:
+    def __init__(self):
+        self.action_space = [0, 1, 2, 3]  # 행동 공간(가능한 행동들)
+        self.action_meaning = {  # 행동의 의미
+            0: 'UP',
+            1: 'DOWN',
+            2: 'LEFT',
+            3: 'RIGHT',
+        }
+
+        self.reward_map = np.array(  # 보상 맵(각 좌표의 보상값)
+            [[0, 0, 0, 1.0],
+             [0, None, 0, -1.0],
+             [0, 0, 0, 0]]
+        )
+        self.goal_state = (0, 3)  # 목표 상태(좌표)
+        self.wall_state = (1, 1)  # 벽 상태(좌표)
+        self.start_state = (2, 0)  # 시작 상태(좌표)
+        self.agent_state = self.start_state  # 에이전트 초기 상태(좌표)
+
+    @property
+    def height(self):
+        return len(self.reward_map)
+    
+    @property
+    def width(self):
+        return len(self.reward_map[0])
+    
+    @property
+    def shape(self):
+        return self.reward_map.shape
+    
+    def actions(self):
+        return self.action_space  # [0, 1, 2, 3]
+    
+    def states(self):
+        for h in range(self.height):
+            for w in range(self.width):
+                yield (h, w)
+
+    def next_state(self, state, action):
+        # 이동 위치 계산
+        action_move_map = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        move = action_move_map[action]
+        next_state = (state[0] + move[0], state[1] + move[1])
+        ny, nx = next_state
+
+        # 이동한 위치가 그리드 월드의 테두리 밖이나 벽인가?
+        if nx < 0 or nx >= self.width or ny < 0 or ny >= self.height:
+            next_state = state
+        elif next_state == self.wall_state:
+            next_state = state
+
+        return next_state  # 다음 상태 변환
+    
+    def reward(self, state, action, next_state):
+        return self.reward_map[next_state]
+
+    def render_v(self, v=None, policy=None, print_value=True):
+        renderer = render_helper.Renderer(self.reward_map, self.goal_state,
+                                          self.wall_state)
+        renderer.render_v(v, policy, print_value)
+
+if __name__ == '__main__':
+    env = GridWorld()
+
+    # env.height() 대신 env.height로 사용 가능
+    print(env.height)  # [출력 결과] 3
+    print(env.width)  # [출력 결과] 4
+    print(env.shape)  # [출력 결과] (3, 4)
+
+    for action in env.actions():  # 모든 행동에 순차적으로 접근
+        print(action)
+
+    print('===')
+
+    for state in env.states():  # 모든 상태에 순차적으로 접근
+        print(state)
+
+    env.render_v()
